e35a4cb796ae30d096d5bf928ffbbe60be7084cf
Michael Wu <mw10013@gmail.com>
2012-05-01 23:44:11 -0400

    cond

--- src/clj_sql_mapper/sql.clj
+++ src/clj_sql_mapper/sql.clj
@@ -1,25 +1,25 @@
 (ns clj-sql-mapper.sql
-  "TODO: spacing, cond, set, substitution, foreach, parent.child
+  "TODO: spacing, cond, substitution, foreach, parent.child, refer warnings
 "
-  (:refer-clojure :rename {when core-when})
+  (:refer-clojure :rename {when core-when set core-set cond core-cond})
   (:require [clojure.tools.logging :as log]
             [clojure.string :as str]))
 
 (defn prepare [m sql]
   (reduce (fn [[sql-str sql-params] x]
-            (cond
+            (core-cond
              (string? x) [(str sql-str x) sql-params]
              (keyword? x) [(str sql-str \?) (conj sql-params (m x))]
              (var? x) (let [[s p] (prepare m @x)] [(str sql-str s) (into sql-params p)])
-             :else (let [[s params] (x m)]
-                     [(str sql-str s) (into sql-params params)])))
+             :else (let [[s p] (x m)]
+                     [(str sql-str s) (into sql-params p)])))
           [nil []] sql))
 
 (defn- parse [s]
   (-> (str "'(\"" s "\")")  (str/replace  #":[a-z0-9\-]+|#'[a-z0-9\-]+" #(str \" % \")) read-string eval))
 
 (defn- compile-sql [sql]
-  (cond
+  (core-cond
    (string? sql) (parse sql)
    (var? sql) sql
    :else (-> sql eval list)))
@@ -41,6 +41,23 @@
     (butlast (reduce (fn [[sql-str params first?] [s p]]
                        (let [s (if first? (str/replace s #"^\s*(?i:and|or)\s+" "") s)]
                          [(str sql-str " " s) (into params p) false]))
-                     ["where" [] true] sqls))))
+                     [" where" [] true] sqls))))
 
 (defmacro where [& sqls] `(fn [param-map#] (prepare-where param-map# '~(map sql* sqls))))
+
+(defn prepare-set [m sqls]
+  (let [sqls (->> sqls (map (partial prepare m)) (remove (comp str/blank? first)))
+        [sql-str params] (reduce (fn [[sql-str params] [s p]]
+                                   [(str sql-str " " s) (into params p)])
+                                 [" set" []] sqls)
+        sql-str (str/replace sql-str #"\s*,\s*$" "")]
+    [sql-str params]))
+
+(defmacro set [& sqls] `(fn [param-map#] (prepare-set param-map# '~(map sql* sqls))))
+
+(defn prepare-cond [m sqls]
+  (or (->> sqls (map (partial prepare m)) (drop-while (comp str/blank? first)) first) ["" []]))
+
+(defmacro cond [& sqls] `(fn [param-map#] (prepare-cond param-map# '~(map sql* sqls))))
+
+; (prepare {} (sql (cond (when :title "where title = :title"))))
--- test/clj_sql_mapper/test/sql.clj
+++ test/clj_sql_mapper/test/sql.clj
@@ -9,7 +9,7 @@
   (is (= '("select * from table") (sql/sql "select * from table")))
   (is (= '("select * from table where title = " :title) (sql/sql "select * from table where title = :title"))))
 
-(deftest when
+(deftest sql-when
   (is (= ["where title = ?" ["the-title"]] ((sql/when :title "where title = :title") {:title "the-title"})))
   (is (= ["where title = ?" ["the-title"]] ((sql/when #(:title %) "where title = :title") {:title "the-title"})))
   (is (= ["where title = ?" ["the-title"]] ((sql/when identity "where title = :title") {:title "the-title"}))))
@@ -26,26 +26,44 @@
 (deftest vars
   (is (= ["select col1, col2, col3 from table" []] (sql/prepare {} (sql/sql "select #'cols from table"))))
   (is (= ["select col1, col2, col3 from table where title = ?" ["the-title"]]
-         (sql/prepare {:title "the-title"} (sql/sql "select #'cols from table " (sql/where #'title))))))
+         (sql/prepare {:title "the-title"} (sql/sql "select #'cols from table" (sql/where #'title))))))
 
-(deftest where
+(deftest sql-where
   #_(is (= ["" []] (sql/prepare {} (sql/sql (sql/where)))))
-  (is (= ["where title = ?" ["the-title"]] (sql/prepare {:title "the-title"} (sql/sql (sql/where "title = :title")))))
-  (is (= ["where title = ?" ["the-title"]] (sql/prepare {:title "the-title"} (sql/sql (sql/where "or title = :title")))))
-  (is (= ["where title = ?" ["the-title"]] (sql/prepare {:title "the-title"} (sql/sql (sql/where "and title = :title")))))
-  (is (= ["where title = ?" ["the-title"]] (sql/prepare {:title "the-title"} (sql/sql (sql/where " or title = :title")))))
-  (is (= ["where title = ?" ["the-title"]] (sql/prepare {:title "the-title"} (sql/sql (sql/where " and title = :title")))))
-  (is (= ["where title = ?" ["the-title"]] (sql/prepare {:title "the-title"} (sql/sql (sql/where "OR title = :title")))))
-  (is (= ["where title = ?" ["the-title"]] (sql/prepare {:title "the-title"} (sql/sql (sql/where "AND title = :title")))))
-  (is (= ["where title = ?" ["the-title"]]
+  (is (= [" where title = ?" ["the-title"]] (sql/prepare {:title "the-title"} (sql/sql (sql/where "title = :title")))))
+  (is (= [" where title = ?" ["the-title"]] (sql/prepare {:title "the-title"} (sql/sql (sql/where "or title = :title")))))
+  (is (= [" where title = ?" ["the-title"]] (sql/prepare {:title "the-title"} (sql/sql (sql/where "and title = :title")))))
+  (is (= [" where title = ?" ["the-title"]] (sql/prepare {:title "the-title"} (sql/sql (sql/where " or title = :title")))))
+  (is (= [" where title = ?" ["the-title"]] (sql/prepare {:title "the-title"} (sql/sql (sql/where " and title = :title")))))
+  (is (= [" where title = ?" ["the-title"]] (sql/prepare {:title "the-title"} (sql/sql (sql/where "OR title = :title")))))
+  (is (= [" where title = ?" ["the-title"]] (sql/prepare {:title "the-title"} (sql/sql (sql/where "AND title = :title")))))
+  (is (= [" where title = ?" ["the-title"]]
          (sql/prepare {:title "the-title"} (sql/sql (sql/where (sql/when :author "and author = :author")
                                                                (sql/when :title "and title = :title"))))))
-  (is (= ["where author = ?" ["clinton"]]
+  (is (= [" where author = ?" ["clinton"]]
          (sql/prepare {:author "clinton"} (sql/sql (sql/where (sql/when :author "and author = :author")
                                                                (sql/when :title "and title = :title"))))))
-  (is (= ["where author = ? and title = ?" ["clinton" "the-title"]]
+  (is (= [" where author = ? and title = ?" ["clinton" "the-title"]]
          (sql/prepare {:author "clinton" :title "the-title"} (sql/sql (sql/where (sql/when :author "and author = :author")
-                                                                                 (sql/when :title "and title = :title"))))))
-  )
+                                                                                 (sql/when :title "and title = :title")))))) )
+
+(deftest sql-set
+  (is (= ["update table set col1 = val1" []] (sql/prepare {} (sql/sql "update table" (sql/set "col1 = val1,")))))
+  (is (= [" set title = ?" ["the-title"]] (sql/prepare {:title "the-title"} (sql/sql (sql/set "title = :title,")))))
+  (is (= [" set title = ?, author = ?" ["the-title" "clinton"]]
+         (sql/prepare {:title "the-title" :author "clinton"}
+                      (sql/sql (sql/set (sql/when :title "title = :title,")
+                                        (sql/when :author "author = :author,")))))))
+
+(deftest sql-cond
+  (is (= ["title = ?" ["the-title"]] (sql/prepare {:title "the-title"} (sql/sql (sql/cond (sql/when :title "title = :title")
+                                                                                          (sql/when :author "author = :author")
+                                                                                          "otherwise")))))
+  (is (= ["author = ?" ["clinton"]] (sql/prepare {:author "clinton"} (sql/sql (sql/cond (sql/when :title "title = :title")
+                                                                                        (sql/when :author "author = :author")
+                                                                                        "otherwise")))))
+  (is (= ["otherwise" []] (sql/prepare {} (sql/sql (sql/cond (sql/when :title "title = :title")
+                                                             (sql/when :author "author = :author")
+                                                             "otherwise"))))))
 
 (run-tests 'clj-sql-mapper.test.sql)
