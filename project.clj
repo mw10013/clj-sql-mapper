e58579db0ee511a779e11d279f5a69e45f98e475
Michael Wu <mw10013@gmail.com>
2012-05-10 00:00:15 -0400

    0.0.6

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-sql-mapper "0.0.5"
+(defproject clj-sql-mapper "0.0.6"
   :description "SQL for Clojure with apologies to mybatis"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/java.jdbc "0.1.4"]
