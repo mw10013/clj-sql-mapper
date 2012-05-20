462fce15121d770700fa7021f5c30030a3f98c1f
Michael Wu <mw10013@gmail.com>
2012-05-20 10:00:45 -0400

    0.0.12

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ No actual sql mapper functionality yet.
 Simply add clj-sql-mapper as a dependency to your lein project:
 
 ```clojure
-[org.clojars.mw10013/clj-sql-mapper "0.0.11"]
+[org.clojars.mw10013/clj-sql-mapper "0.0.12"]
 ```
 
 ## Usage
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-sql-mapper "0.0.11"
+(defproject clj-sql-mapper "0.0.12"
   :description "SQL for Clojure with apologies to mybatis"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/java.jdbc "0.2.1"]
