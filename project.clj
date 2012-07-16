9ef6f0ba923dc45390913c3a2684754dc62af4e8
Michael Wu <mw10013@gmail.com>
2012-07-16 00:11:01 -0400

    reduce-rows

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject clj-sql-mapper "0.0.12"
   :description "SQL for Clojure with apologies to mybatis"
   :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/core.incubator "0.1.1"]
                  [org.clojure/java.jdbc "0.2.2"]
                  [c3p0/c3p0 "0.9.1.2"]]
   :dev-dependencies [[org.hsqldb/hsqldb "2.2.8"]])
--- src/clj_sql_mapper/mapper.clj
+++ src/clj_sql_mapper/mapper.clj
@@ -1,4 +1,5 @@
-(ns clj-sql-mapper.mapper)
+(ns clj-sql-mapper.mapper
+  (:use [clojure.core.incubator :only [dissoc-in]]))
 
 (defn make-mapping
   "Takes f, a function of one arg that returns the arg mapped,
@@ -31,7 +32,46 @@
     (reduce-mappings mappings x)
     (map (partial reduce-mappings mappings) x)))
 
+(defn- result-path [m path]
+  (let [result-path (reduce
+                     (fn [p k]
+                       (let [p (conj p k)
+                             coll (get-in m p)]
+                         (when-not (vector? coll)
+                           (throw (Exception. (str "result-path: expected vector at " p " instead of " coll))))
+                         (when (= (count coll) 0) (throw (Exception. (str "result-path: coll at " p " is empty."))))
+                         (conj p (-> coll count dec))))
+         [:result] (butlast path))]
+    (conj result-path (last path))))
+
+(defn- reduce-row [m r]
+  (reduce
+   (fn [m {:keys [path match-val-fn ks]}]
+     (let [v (match-val-fn r)
+           match-path (list* :matches path)]
+       (if (= v (get-in m (concat match-path [::v])))
+         m
+         (if (nil? v)
+           (dissoc-in m match-path)
+           (-> m
+               (update-in match-path (fnil assoc {}) ::v v)
+               (update-in (result-path m path) (fnil conj []) (select-keys r ks)))))))
+   m (:matchers m)))
+
+(defn reduce-rows [matchers rows]
+  (:result (reduce reduce-row {:matchers matchers} rows)))
+
 (comment
+  (reduce-rows {:row-key :as :match-val-fn :a :ks [:a]
+                :children [{:row-key :bs :match-val-fn :b :ks [:b]}
+                           {:row-key :cs :match-val-fn :c :ks [:c]}]}
+               [{:a 1 :b 1} {:a 1 :b 2} {:a 2 :b 1} {:a 2 :c 3}])
+  
+  (println (reduce-rows [{:path [:as] :match-val-fn :a :row-key :as :ks [:a]}
+                         {:path [:as :bs] :match-val-fn :b :row-key :bs :ks [:b]}
+                         {:path [:as :cs] :match-val-fn :c :row-key :cs :ks [:c]}]
+                        [{:a 1 :b 1} {:a 1 :b 2} {:a 2 :b 1} {:a 2 :c 3}]))
+  
   (apply-mappings [(fn [m] (update-in m [:a] inc))] {:a 1})
   (apply-mappings [(fn [m] (update-in m [:a] inc))] [{:a 1} {:a 11}])
 
