cb1e87e94c9a267aac4a380dc0a2f1c1802f2097
Michael Wu <mw10013@gmail.com>
2012-08-12 13:20:41 -0400

    add bootstrap to html table

--- src/clj_sql_mapper/browse.clj
+++ src/clj_sql_mapper/browse.clj
@@ -1,6 +1,7 @@
 (ns clj-sql-mapper.browse
   (:use [clojure.java.browse :only [browse-url]]
-        [hiccup.core :only [html]]))
+        [hiccup [core :only [html]] [page :only [include-css]]])
+  (:require [clojure.data.xml :as xml]))
 
 (defn- html-row [ks m]
   [:tr (map #(vector :td (m %)) ks)])
@@ -9,10 +10,16 @@
   (when-let [m (first coll)]
     (let [ks (->> m keys (apply sorted-set))]
       (html
-       [:table {:border 1}
-        [:tr
-         (map #(vector :th %) ks)]
-        (map (partial html-row ks) coll)]))))
+       [:head
+        (include-css "http://twitter.github.com/bootstrap/assets/css/bootstrap.css")]
+       [:div.container-fluid
+        [:p "&nbsp;"]
+        [:table.table.table-bordered.table-striped.table-condensed
+         [:thead
+          [:tr
+           (map #(vector :th %) ks)]]
+         [:tbody
+          (map (partial html-row ks) coll)]]]))))
 
 (defn browse
   "Browse a coll of maps as an html table in a browser."
@@ -23,4 +30,11 @@
       (spit file html)
       (browse-url (str "file:///" (.getAbsolutePath file))))))
 
-; (browse [{:b 2 :c 3 :a 1} {:b 22 :c 33 :a 11}])
+; (println (xml/indent-str (xml/as-elements [{:b 2 :c 3 :a 1} {:b 22 :c 33 :a 11}])))
+; (println (xml/indent-str (xml/as-elements {:b 2 :c 3 :a 1})))
+
+(comment
+  (browse [{:b 2 :c 3 :a 1} {:b 22 :c 33 :a 11}])
+  )
+
+
